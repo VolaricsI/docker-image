2a9ee1a9b29b0284852fffe865cbe67af9e01e24
Volarics István <volaricsi@tu20.nker.volarics.hu>
2018-10-09 14:24:13 +0200

    a btsync (resilio) image alpine + glibc alapon

---
+++ docker-btsync/README.md
@@ -0,0 +1,16 @@
+
+    alpine alapon, glibc-vel: resilio
+
+
+- ``-v /home/btsync/config: /config``: Beállításai és a saját adatbázisa, ha nincs config file akkor egy alapértelmezettel indul
+
+- ``-v /home/btsync/data: /downloads``: Adatok helye
+
+- ``-p 8888:8888``: Konfiguráció http-n keresztül
+
+- ``-p 55555:55555``: Adat csere portja
+
+-  -e PUID : user  ID
+-  -e GUID : group ID
+
+-  -e UMASK 	: umask értéke; én a 002-öt szeretem
---
+++ docker-btsync/defaults/start.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+CONFIGDIR=/config
+DATADIR=/downloads
+
+    2>&1
+
+# set deluge user and group id
+USERNAME=btsync
+D_UID=${PUID:=1000}
+D_GID=${PGID:=1000}
+
+echo "Setting User and owner for config and data directories."
+id $USERNAME 2>/dev/null && deluser  $USERNAME			## Ha van ilyen user vagy group töröljük,
+id -g $D_GID 2>/dev/null && delgroup $USERNAME
+addgroup -S -g $D_GID $USERNAME					## hogy megfelelő ID-kel újra létrehozhassuk
+adduser  -S -D -h /dev/null -u $D_UID -G $USERNAME $USERNAME
+
+chown -R $D_UID:$D_GID $CONFIGDIR
+chown    $D_UID $DATADIR
+
+if [ ! -f $CONFIGDIR/sync.conf  ]; then				## Ha nincs konfig akkor kap egy alapértelmezettet
+        echo "Doing initial setup."
+        # Starting deluge
+	cp /defaults/sync.conf.example $CONFIGDIR/sync.conf.example
+	cp /defaults/sync.conf.example $CONFIGDIR/sync.conf
+	chown -R $D_UID:$D_GID $CONFIGDIR
+
+fi
+
+/usr/bin/rslsync --help |grep "Resilio Sync"		## a verzio kiíratás miatt
+
+echo "Starting resilio..."
+umask $UMASK 	>/dev/null
+
+exec chpst -u $USERNAME /usr/bin/rslsync --nodaemon --config $CONFIGDIR/sync.conf
