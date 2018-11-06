dc4ed5cecd570d529c044833057ee7970978a9b9
Volarics István <volaricsi@tu20.nker.volarics.hu>
2018-11-06 15:40:06 +0100

    Nginx induláskor létrehozza, de nincs használatban így letörlöm (nem kell a conténerbe)

--- rtorrent-rutorrent/defaults/startup-rtorrent.sh
+++ rtorrent-rutorrent/defaults/startup-rtorrent.sh
@@ -6,6 +6,9 @@ CONFIG_FILE=/config/rtorrent/rtorrent.rc
 
 USERNAME=torrent
 
+    supervisorctl status nginx
+    supervisorctl status nginx |grep RUNNING && [ -e /var/log/nginx/error.log ] && rm /var/log/nginx/error.log	## Ne legyen itt hiszen nem használjuk
+
 					## Ha fut leállítom, de mindenképp törlöm a lock file-t mivel ha van akkor nem fog elindulni
     Lock_File=$( grep "session = " ${CONFIG_FILE} |sed 's/.*[ =]//g;' )/rtorrent.lock
     if [ -e ${Lock_File} ]; then
