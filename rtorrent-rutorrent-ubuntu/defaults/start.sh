8f23e13b6dbf89f910259058fdb168318b5a69a0
Volarics István <volaricsi@tu20.nker.volarics.hu>
2018-11-29 07:07:10 +0100

    A felhasználó neve egységesítve (abc)

--- rtorrent-rutorrent-ubuntu/defaults/start.sh
+++ rtorrent-rutorrent-ubuntu/defaults/start.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-CONFIGDIR=/config
-DATADIR=/downloads
-
-# set user name, user id and group id
-USERNAME=rtorrent
+# set user id and group id
 D_UID=${PUID:=1000}
 D_GID=${PGID:=1000}
 
 echo "Setting user and owner for config and data directories."
-userdel  --force $USERNAME 2>/dev/null							##Mindenképpen töröljük,
-groupdel -f 	 $USERNAME 2>/dev/null
-groupadd -g $D_GID $USERNAME								## hogy a megfelelő ID-vel újra létrehozhassuk
-adduser --system --no-create-home --home /nonexistent --uid $D_UID --gid $D_GID $USERNAME
+userdel  --force abc 2>/dev/null						##Mindenképpen töröljük,
+groupdel --force abc 2>/dev/null
+groupadd -g $D_GID abc								## hogy a megfelelő ID-vel újra létrehozhassuk
+##adduser --system --no-create-home --home /tmp --uid $D_UID --gid $D_GID abc
+adduser --system --no-create-home --uid $D_UID --gid $D_GID abc
 
-mkdir -p $CONFIGDIR
-mkdir -p $DATADIR
-chown -R $D_UID:$D_GID $CONFIGDIR
-chown    $D_UID $DATADIR
-
-
-[ ".DISABLE_PLUGINS" != "." ] /defaults/disable_plugins $DISABLE_PLUGINS		## Plugins beállítás kívülről
+mkdir -p /config /downloads
+chown -R $D_UID:$D_GID 	/config
+chown    $D_UID 	/downloads
 
 
-    ln -s /proc/1/fd/1 /dev/console		## A run-it nem szól a hiánya miatt
+[ ".$DISABLE_PLUGINS" != "." ] && /defaults/disable_plugins $DISABLE_PLUGINS		## Plugins beállítás kívülről
+
+
+    [ ! -e /dev/console ] && ln -s /proc/1/fd/1 /dev/console		## A run-it így nem szól a hiánya miatt
+
     2>&1
 	exec /sbin/runit
