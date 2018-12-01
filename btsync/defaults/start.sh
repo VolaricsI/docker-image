8121c6464eb7b0b2954405ec9b79d63419b045c1
Volarics István <volaricsi@tu20.nker.volarics.hu>
2018-12-01 13:52:54 +0100

    Kihagyva a változókat és használva a create_default_config prg-t

--- btsync/defaults/start.sh
+++ btsync/defaults/start.sh
@@ -1,40 +1,34 @@
 #!/bin/sh
 
-CONFIGDIR=/config
-DATADIR=/downloads
-
-    2>&1
-
 # set deluge user and group id
-USERNAME=btsync
 D_UID=${PUID:=1000}
 D_GID=${PGID:=1000}
 
 echo "Setting User and owner for config and data directories."
-id $USERNAME 2>/dev/null && deluser  $USERNAME			## Ha van ilyen user vagy group töröljük,
-id -g $D_GID 2>/dev/null && delgroup $USERNAME
-addgroup -S -g $D_GID $USERNAME					## hogy megfelelő ID-kel újra létrehozhassuk
-adduser  -S -D -h /dev/null -u $D_UID -G $USERNAME $USERNAME
+id abc 		2>/dev/null && deluser  abc			## Ha van ilyen user vagy group töröljük,
+id -g $D_GID 	2>/dev/null && delgroup abc
+addgroup -S -g $D_GID abc 					## hogy megfelelő ID-kel újra létrehozhassuk
+adduser  -S -D -H -h /tmp -u $D_UID -G abc abc
 
-chown -R $D_UID:$D_GID $CONFIGDIR
-chown    $D_UID $DATADIR
 
-if [ ! -f $CONFIGDIR/sync.conf  ]; then				## Ha nincs konfig akkor kap egy alapértelmezettet
+if [ ! -f /config/sync.conf  ]; then				## Ha nincs konfig akkor kap egy alapértelmezettet
         echo "Doing initial setup."
-        # Starting deluge
-	cp /defaults/sync.conf.example $CONFIGDIR/sync.conf.example
-	cp /defaults/sync.conf.example $CONFIGDIR/sync.conf
-	chown -R $D_UID:$D_GID $CONFIGDIR
-
+	cd /config
+	create_default_config >sync.conf.example
+	cp sync.conf.example sync.conf
 fi
 
-    cd /tmp
 
-    /usr/bin/rslsync --help |grep "Resilio Sync"		## a verzio kiíratás miatt
+chown -R $D_UID:$D_GID 	/config
+chown    $D_UID 	/downloads
 
-    rm -rf /tmp/.sync /tmp/*	# Az előző sor feleslegét takarítja el....
 
     echo "Starting resilio..."
+    cd /tmp
+    /usr/bin/rslsync --help |grep "Resilio Sync"		## a verzio kiíratás miatt
+    rm -rf /tmp/.sync /tmp/*
+
+
     umask $UMASK
 
-    exec chpst -u $USERNAME /usr/bin/rslsync --nodaemon --config $CONFIGDIR/sync.conf
+    exec chpst -u abc /usr/bin/rslsync --nodaemon --config /config/sync.conf
