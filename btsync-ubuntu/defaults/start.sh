696d739f50073b786de6878441b7f5777f0bb61d
Volarics István <volaricsi@tu20.nker.volarics.hu>
2018-12-01 14:49:13 +0100

    Egységesítéssel került ide

--- btsync-ubuntu/defaults/start.sh
+++ btsync-ubuntu/defaults/start.sh
@@ -1,33 +1,40 @@
-#!/bin/bash
-
-CONFIGDIR=/config
-DATADIR=/downloads
+#!/bin/sh
 
 # set deluge user and group id
-USERNAME=btsync
 D_UID=${PUID:=1000}
 D_GID=${PGID:=1000}
 
-echo "Setting user and owner for config and data directories."
-userdel  --force $USERNAME 2>/dev/null							##Mindenképpen töröljük,
-groupdel -f 	 $USERNAME 2>/dev/null
-groupadd -g $D_GID $USERNAME								## hogy a megfelelő ID-vel újra létrehozhassuk
-adduser --system --no-create-home --home /nonexistent --uid $D_UID --gid $D_GID $USERNAME
+echo "Setting User and owner for config and data directories."
+id abc 		2>/dev/null && deluser  abc 								## Ha van ilyen user vagy group töröljük,
+id -g $D_GID 	2>/dev/null && delgroup abc
 
-chown -R $D_UID:$D_GID $CONFIGDIR
-chown    $D_UID $DATADIR
-
-if [ ! -f $CONFIGDIR/sync.conf  ]; then							## Ha nincs konfig akkor kap egy alapot
-        echo "Doing initial setup."
-        # Starting deluge
-	cp /defaults/sync.conf.example $CONFIGDIR/sync.conf.example
-	cp $CONFIGDIR/sync.conf.example $CONFIGDIR/sync.conf
-	chown -R $D_UID:$D_GID $CONFIGDIR
-
+if [ $ALAP == "ubuntu" ]; then 										## hogy megfelelő ID-kel újra létrehozhassuk
+    addgroup --system -gid $D_GID abc
+    adduser --system  --disabled-password --no-create-home --home /tmp -uid $D_UID -gid $D_GID abc
+else
+    addgroup -S -g $D_GID abc 					## hogy megfelelő ID-kel újra létrehozhassuk
+    adduser  -S -D -H -h /tmp -u $D_UID -G abc abc
 fi
 
-/bin/rslsync --help	## a verzio szám kiíratás miatt
 
-echo "Starting resilio."
-umask $UMASK 	>/dev/null
-exec chpst -u $USERNAME 	/bin/rslsync --nodaemon --config $CONFIGDIR/sync.conf
+if [ ! -f /config/sync.conf  ]; then				## Ha nincs konfig akkor kap egy alapértelmezettet
+        echo "Doing initial setup."
+	cd /config
+	create_default_config >sync.conf.example
+	cp sync.conf.example sync.conf
+fi
+
+
+chown -R $D_UID:$D_GID 	/config
+chown    $D_UID 	/downloads
+
+
+    echo "Starting resilio..."
+    cd /tmp
+    /usr/bin/rslsync --help |grep "Resilio Sync"		## a verzio kiíratás miatt
+    rm -rf /tmp/.sync /tmp/*
+
+
+    umask $UMASK
+
+    exec chpst -u abc /usr/bin/rslsync --nodaemon --config /config/sync.conf
