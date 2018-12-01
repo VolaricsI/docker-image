e537d460896c198cb80db1ca517bc3dbe1624e00
Volarics István <volaricsi@tu20.nker.volarics.hu>
2018-12-01 14:48:04 +0100

    Felkészítve több (alpine, ubuntu) feletti működésre

--- btsync/defaults/start.sh
+++ btsync/defaults/start.sh
@@ -5,10 +5,16 @@ D_UID=${PUID:=1000}
 D_GID=${PGID:=1000}
 
 echo "Setting User and owner for config and data directories."
-id abc 		2>/dev/null && deluser  abc			## Ha van ilyen user vagy group töröljük,
+id abc 		2>/dev/null && deluser  abc 								## Ha van ilyen user vagy group töröljük,
 id -g $D_GID 	2>/dev/null && delgroup abc
-addgroup -S -g $D_GID abc 					## hogy megfelelő ID-kel újra létrehozhassuk
-adduser  -S -D -H -h /tmp -u $D_UID -G abc abc
+
+if [ $ALAP == "ubuntu" ]; then 										## hogy megfelelő ID-kel újra létrehozhassuk
+    addgroup --system -gid $D_GID abc
+    adduser --system  --disabled-password --no-create-home --home /tmp -uid $D_UID -gid $D_GID abc
+else
+    addgroup -S -g $D_GID abc 					## hogy megfelelő ID-kel újra létrehozhassuk
+    adduser  -S -D -H -h /tmp -u $D_UID -G abc abc
+fi
 
 
 if [ ! -f /config/sync.conf  ]; then				## Ha nincs konfig akkor kap egy alapértelmezettet
