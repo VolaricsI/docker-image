d76ed48d6f265d4a7cb1b08adae80154829db750
Volarics István <volaricsi@tu20.nker.volarics.hu>
2018-12-03 11:11:05 +0100

    változók kivezetése

--- rtorrent-rutorrent/defaults/startup-rtorrent.sh
+++ rtorrent-rutorrent/defaults/startup-rtorrent.sh
@@ -1,23 +1,19 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-set -x
-
-CONFIG_FILE=/config/rtorrent/rtorrent.rc
-
-USERNAME=torrent
+##CONFIG_FILE=/config/rtorrent.rc
 
     supervisorctl status nginx
-    supervisorctl status nginx |grep RUNNING && [ -e /var/log/nginx/error.log ] && rm /var/log/nginx/error.log	## Ne legyen itt hiszen nem használjuk
+##    supervisorctl status nginx |grep RUNNING && [ -e /var/log/nginx/error.log ] && rm /var/log/nginx/error.log	## Ne legyen itt hiszen nem használjuk
+
 
 					## Ha fut leállítom, de mindenképp törlöm a lock file-t mivel ha van akkor nem fog elindulni
-    Lock_File=$( grep "session = " ${CONFIG_FILE} |sed 's/.*[ =]//g;' )/rtorrent.lock
+    Lock_File=$( grep "session = " /config/rtorrent.rc |sed 's/.*[ =]//g;' )/rtorrent.lock
     if [ -e ${Lock_File} ]; then
 	kill $(cat ${Lock_File} |sed 's/.*+//g')
 	rm ${Lock_File}
     fi
 
-    2>&1
     cd /tmp
     umask $UMASK 			# Csak ha van UMASK, különben csak kiírja
 
-    exec chpst -u $USERNAME:$USERNAME /bin/sh -c "TERM=xterm exec /usr/bin/rtorrent -n -o import=${CONFIG_FILE}"
+    exec chpst -u abc:abc /bin/sh -c "TERM=xterm exec /usr/bin/rtorrent -n -o import=/config/rtorrent.rc "
