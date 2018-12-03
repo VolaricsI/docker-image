23d6cbb8c165b4d0b3ea93519a268d964269fe94
Volarics István <volaricsi@tu20.nker.volarics.hu>
2018-12-03 11:08:10 +0100

    Új user hozzáadás és át lett nézve a forma és jogosultság állítás

--- rtorrent-rutorrent/defaults/start.sh
+++ rtorrent-rutorrent/defaults/start.sh
@@ -1,45 +1,49 @@
 #!/bin/sh
 
-CONFIGDIR=/config
-DATADIR=/downloads
-SETTING_RUTORRENT_DIR=$CONFIGDIR/rutorrent_settings
-
-# set user name, user id and group id
-USERNAME=torrent
+# set deluge user and group id
 D_UID=${PUID:=1000}
 D_GID=${PGID:=1000}
 
 echo "Setting User and owner for config and data directories."
-id $USERNAME 2>/dev/null && deluser  $USERNAME			## Ha van ilyen user vagy group töröljük,
-id -g $D_GID 2>/dev/null && delgroup $USERNAME
-addgroup -S -g $D_GID $USERNAME					## hogy megfelelő ID-kel újra létrehozhassuk
-adduser  -S -D -h /dev/null -u $D_UID -G $USERNAME $USERNAME
+id abc 		2>/dev/null && deluser  abc 								## Ha van ilyen user vagy group töröljük,
+id -g $D_GID 	2>/dev/null && delgroup abc
 
-mkdir -p $CONFIGDIR $DATADIR
-chown -R $D_UID:$D_GID $CONFIGDIR
-chown    $D_UID $DATADIR
-
-if [ ! -e /config/rtorrent/rtorrent.rc ]; then			## Ha nincs konfig akkor kap egy alapot + exapmle-ket
-    mkdir -p /config/rtorrent
-    cp /defaults/*.example /config/
-    mv /config/rtorrent.rc.example /config/rtorrent/rtorrent.rc
+if [ "$ALAP" = "ubuntu" ]; then 									## hogy megfelelő ID-kel újra létrehozhassuk
+    addgroup --system -gid $D_GID abc
+    adduser --system  --disabled-password --no-create-home --home /tmp -uid $D_UID -gid $D_GID abc
+else
+    addgroup -S -g $D_GID abc
+    adduser  -S -D -H -h /tmp -u $D_UID -G abc abc
 fi
 
-								## Az rtorrent-nek kellenek ezek a könyvtárak
-    Dir_Session=$( grep "session = "      /config/rtorrent/rtorrent.rc |sed 's/.*[ =]//g;' )
-    Dir_Watch=$(   grep "watch_directory" /config/rtorrent/rtorrent.rc |sed 's/.*=//g; s/\/\*.*//g; ' )
+
+
+if [ ! -e /config/rtorrent.rc ]; then			## Ha nincs konfig akkor kap egy alapot + exapmle-ket
+    cp /defaults/*.example /config/
+    cp /config/rtorrent.rc.example /config/rtorrent.rc
+fi
+
+							## Az rtorrent-nek kellenek ezek a könyvtárak
+    Dir_Session=$( grep "session = "      /config/rtorrent.rc |sed 's/.*[ =]//g;' )
+    Dir_Watch=$(   grep "watch_directory" /config/rtorrent.rc |sed 's/.*=//g; s/\/\*.*//g; ' )
     mkdir -p ${Dir_Session} ${Dir_Watch}
-    chown $USERNAME:$USERNAME -R /config
 
-								## A rutorrent mentéseinek helye
-    [ ! -d $SETTING_RUTORRENT_DIR ] && mkdir -p $SETTING_RUTORRENT_DIR
-    chown -R nginx:www-data $SETTING_RUTORRENT_DIR
 
-								## Plugin-ok kívülről történő beállítása
-[ ".$DISABLE_PLUGINS" != "." ] && /defaults/disable_plugins "$DISABLE_PLUGINS"		## Plugins beállítás kívülről
-[ ".$INSTALL"         != "." ] && apk add ffmpeg sox mediainfo unrar			## Minden további csomagot felrak
+							## A rutorrent mentéseinek helye
+    [ ! -d /config/rutorrent_settings ] && mkdir -p /config/rutorrent_settings
+    [ ! -d /config/rutorrent_watched  ] && mkdir -p /config/rutorrent_watched
+
+
+		# Jól álljanak a jogosultságok
+    chown -RL $D_UID:$D_GID 	/config
+    chown -RL $D_UID 		/downloads
+    chown -RL nginx:www-data 	/var/www
+
+							## Plugin-ok kívülről történő beállítása
+[ ".$DISABLE_PLUGINS" != "." ] && disable_plugins "$DISABLE_PLUGINS"		## Plugins beállítás kívülről
+[ ".$INSTALL"         != "." ] && apk add $PLUGIN_PRG_LIST			## Minden további csomagot felrak
 
     echo Starting supervisord...
 
-    /usr/bin/supervisord					## Ezt használom az exec helyett mert így eltűnik egy hibaüzenet: "...reaped unknown pid..."
-#    exec /usr/bin/supervisord -c /etc/supervisord.conf
+#exec /usr/bin/supervisord
+    /usr/bin/supervisord					## Ezt használom az exec helyett mert így eltűnik egy hibaüzenet: "...INFO reaped unknown pid..."
