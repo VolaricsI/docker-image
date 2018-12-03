41b443a26450399fc163fa4f23efb9fff5dfb8f8
Volarics István <volaricsi@tu20.nker.volarics.hu>
2018-12-03 15:30:46 +0100

    Az adduser-abc alkalmazása/használatbavétele

--- rtorrent-rutorrent/defaults/start.sh
+++ rtorrent-rutorrent/defaults/start.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
+#
+#	Create by Voli
 
-# set deluge user and group id
-D_UID=${PUID:=1000}
-D_GID=${PGID:=1000}
-
-echo "Setting User and owner for config and data directories."
-id abc 		2>/dev/null && deluser  abc 								## Ha van ilyen user vagy group töröljük,
-id -g $D_GID 	2>/dev/null && delgroup abc
-
-if [ "$ALAP" = "ubuntu" ]; then 									## hogy megfelelő ID-kel újra létrehozhassuk
-    addgroup --system -gid $D_GID abc
-    adduser --system  --disabled-password --no-create-home --home /tmp -uid $D_UID -gid $D_GID abc
-else
-    addgroup -S -g $D_GID abc
-    adduser  -S -D -H -h /tmp -u $D_UID -G abc abc
-fi
-
+    /defaults/adduser-abc || exit 1		## Ha kell akkor ujra alkotjuk az abc felhasználót
 
 
 if [ ! -e /config/rtorrent.rc ]; then			## Ha nincs konfig akkor kap egy alapot + exapmle-ket
@@ -35,8 +22,8 @@ fi
 
 
 		# Jól álljanak a jogosultságok
-    chown -RL $D_UID:$D_GID 	/config
-    chown -RL $D_UID 		/downloads
+    chown -RL abc:abc 	/config
+    chown -RL abc 	/downloads
     chown -RL abc:abc 	/var/www
 
 							## Plugin-ok kívülről történő beállítása
