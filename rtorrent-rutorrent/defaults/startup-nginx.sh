7917bad2a882d812e93d20f24f36bd16b4a5ae52
Volarics István <volaricsi@tu20.nker.volarics.hu>
2018-12-03 11:12:09 +0100

    Nincs jelszó kezelés; oldják meg saját configgal

--- rtorrent-rutorrent/defaults/startup-nginx.sh
+++ rtorrent-rutorrent/defaults/startup-nginx.sh
@@ -1,13 +1,4 @@
-#!/usr/bin/env sh
-set -x
-
-CONF_FILE=/etc/nginx/sites-enabled/nginx-rutorrent.conf
-PWD_FILE=/config/htpasswd
-
-## Hitelesítés beállítása: Ha nincs saját konfig és létezik a htpasswd file
-    [ -e /config/nginx.conf ] || [ -e $PWD_FILE ] && sed -i 's/.*\(auth_basic .*\)/\1/g;     s/.*\(auth_basic_user_file \)/\1/g; ' 	$CONF_FILE
-    [ -e /config/nginx.conf ] || [ -e $PWD_FILE ] || sed -i 's/.*\(auth_basic .*\)/##\t\1/g; s/.*\(auth_basic_user_file \)/##\t\1/g; ' 	$CONF_FILE
-
+#!/bin/sh
 
     [ -e /config/nginx.conf ] && EXTERNAL_CONF_FILE=" -c /config/nginx.conf "
 
