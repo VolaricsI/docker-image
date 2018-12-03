96c004f5bc2959f0fb4b603a92513918e43b4aa3
Volarics István <volaricsi@tu20.nker.volarics.hu>
2018-12-03 11:14:51 +0100

    Nincs memória állításra lehetőség; és a symlinken (php-fmp) keresztül indítjuk az eredeti programot

--- rtorrent-rutorrent/defaults/startup-php.sh
+++ rtorrent-rutorrent/defaults/startup-php.sh
@@ -1,14 +1,7 @@
-#!/usr/bin/env sh
-
-set -x
-
-				## A konténeren kívülről érkezik; Saját conf-nál ezt nem kell beállítani
-[ ".$PHP_MEM" != "." ] && sed -i 's/memory_limit.*$/memory_limit = '$PHP_MEM'/g' /etc/php7/php.ini
-[ ".$PHP_MEM" != "." ] && sed -i 's/memory_limit.*$/memory_limit = '$PHP_MEM'/g' /etc/php7/php-fpm.conf
-
+#!/bin/sh
 
     [ -e /config/php-fpm-rutorrent.conf ] && CONF_FILE=" --fpm-config /config/php-fpm-rutorrent.conf "
 
-    mkdir -p /run/php /var/run/php
+    mkdir -p /run/php	##Néha kellhet
 
-    exec php-fpm7 --nodaemonize --force-stderr $CONF_FILE
+    exec php-fpm --nodaemonize --force-stderr $CONF_FILE
