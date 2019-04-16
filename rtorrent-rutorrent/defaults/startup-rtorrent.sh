59c94e4fadf04a19d844a2e3408d976bf147143a
Volarics István <volaricsi@volarics.hu>
2019-04-16 09:38:23 +0200

    Ezt már itt nem kell mivel a konténer indulásakor le lett rendezve

--- rtorrent-rutorrent/defaults/startup-rtorrent.sh
+++ rtorrent-rutorrent/defaults/startup-rtorrent.sh
@@ -6,12 +6,12 @@
 ##    supervisorctl status nginx |grep RUNNING && [ -e /var/log/nginx/error.log ] && rm /var/log/nginx/error.log	## Ne legyen itt hiszen nem használjuk
 
 
-					## Ha fut leállítom, de mindenképp törlöm a lock file-t mivel ha van akkor nem fog elindulni
-    Lock_File=$( grep "session.*=" /config/rtorrent.rc |sed 's/.*[ =]//g;' )/rtorrent.lock
-    if [ -e ${Lock_File} ]; then
-	kill $( cat ${Lock_File} |sed 's/.*+//g' ) 	2>/dev/null
-	rm ${Lock_File}
-    fi
+#					## Ha fut leállítom, de mindenképp törlöm a lock file-t mivel ha van akkor nem fog elindulni
+#    Lock_File=$( grep "session.*=" /config/rtorrent.rc |sed 's/.*[ =]//g;' )/rtorrent.lock
+#    if [ -e ${Lock_File} ]; then
+#	kill $( cat ${Lock_File} |sed 's/.*+//g' ) 	2>/dev/null
+#	rm ${Lock_File}
+#    fi
 
     cd /tmp
     umask $UMASK 			# Csak ha van UMASK, különben csak kiírja
