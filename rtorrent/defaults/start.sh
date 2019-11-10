05d665551ec9eb1aee58090610f1169fc4bd63d4
Volarics István <volaricsi@volarics.hu>
2019-11-10 14:38:43 +0100

    A runit indítói

--- rtorrent/defaults/start.sh
+++ rtorrent/defaults/start.sh
@@ -4,16 +4,14 @@
 
     if [ .$( cat /downloads/.docker-mount-point ) != ./downloads ]; then		## Csak akkor fusson ha VALÓBAN a megfelelő könyvtár van alatta
 	echo "Nem megfelelő a csatolt könyvtár...."
-	exit 1
+#	exit 1
     fi
 
 
     /defaults/adduser-abc || exit 1		## Ha kell akkor ujra alkotjuk az abc felhasználót
 
-#		# Jól álljanak a jogosultságok
-#    chown -RLc abc:abc 	/config
-#    chown -RLc abc 	/downloads
+		# Jól álljanak a jogosultságok
+    chown -RLc abc:abc 	/config
+    chown -RLc abc 	/downloads
 
-    /defaults/start-lighttpd.sh &
-
-    exec /defaults/start-rtorrent.sh
+    exec /sbin/runsvdir /etc/service
