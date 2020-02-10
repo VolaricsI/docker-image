60502463670fe9183e48017ae2a561008de1a26a
Volarics István <volaricsi@tu20.nker.volarics.hu>
2020-02-10 09:27:53 +0100

    A böngésző fejlécében megjelenik a  globális vátozó

--- rtorrent/defaults/start.sh
+++ rtorrent/defaults/start.sh
@@ -4,11 +4,16 @@
 
     if [ .$( cat /downloads/.docker-mount-point ) != ./downloads ]; then		## Csak akkor fusson ha VALÓBAN a megfelelő könyvtár van alatta
 	echo "Nem megfelelő a csatolt könyvtár...."
-#	exit 1
+	exit 1
     fi
 
 
     /defaults/adduser-abc || exit 1		## Ha kell akkor ujra alkotjuk az abc felhasználót
+
+
+		##Ha meg van adva akkor ez lesz a web-es felület fejléce (title)
+    [ -n "$[TITLE}" ] && sed -i "s/newTitle+=\"ruTorrent/newTitle+=\"${TITLE}/" /var/www/ruTorrent/js/webui.js
+
 
 		# Jól álljanak a jogosultságok
     chown -RLc abc:abc 	/config
