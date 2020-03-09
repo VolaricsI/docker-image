868c04b8361e297e938be8f19499548c5c260432
Volarics István <volaricsi@tu20.nker.volarics.hu>
2020-03-09 11:13:12 +0100

    A default logok egységesen a /tmp-be átírányítva

--- rtorrent/defaults/configure.sh
+++ rtorrent/defaults/configure.sh
@@ -7,7 +7,7 @@
 	ln -nf /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime && echo "${TIME_ZONE}" >/etc/timezone || exit 1
     fi
 	## Az adatbázis nem kell; viszont ubuntu alatt kell a fent lévő csomagoknak!!
-    [ -x /sbin/apk 		] && apk del tzdata
+    [ -x /sbin/apk ] && apk del tzdata
 
 	# futtatható programok beállítása és helyre mozgatása
     cd /defaults 	\
@@ -16,34 +16,28 @@
     && mv start.sh / 	\
     || exit 2
 
-	## supervisord sock file-ja
-    rm /var/run/supervisor.sock 2>/dev/null; touch /tmp/supervisor.sock; ln -s /tmp/supervisor.sock /var/run/supervisor.sock
-
-
 	## a default log-okat induláskor létrehozza/megnyitja és így (ha csinálja) akkor /tmp-ben változtat
     mkdir -p /var/log/php7
-    rm /var/log/php-fpm.log 	2>/dev/null; touch /tmp/php-fpm.log; 		ln -s /tmp/php-fpm.log 		/var/log/php-fpm.log
-    rm /var/log/php7/error.log 	2>/dev/null; touch /tmp/php_error.log; 		ln -s /tmp/php_error.log 	/var/log/php7/error.log
-    rm /var/log/nginx/error.log 2>/dev/null; touch /tmp/nginx_error.log; 	ln -s /tmp/nginx_error.log 	/var/log/nginx/error.log
+    rm /var/log/php-fpm.log 		2>/dev/null; touch /tmp/php-fpm.log; 		ln -s /tmp/php-fpm.log 		/var/log/php-fpm.log
+    rm /var/log/php7/error.log 		2>/dev/null; touch /tmp/php_error.log; 		ln -s /tmp/php_error.log 	/var/log/php7/error.log
+    rm /var/log/nginx/error.log 	2>/dev/null; touch /tmp/nginx_error.log; 	ln -s /tmp/nginx_error.log 	/var/log/nginx/error.log
+    rm /var/log/nginx/access.log 	2>/dev/null; touch /tmp/nginx_access.log; 	ln -s /tmp/nginx_access.log 	/var/log/nginx/access.log
 
-	## A php-fpm futtatóját beállítom
-    PhpFpmNeve=$( find /usr/sbin/ -executable|grep php-fpm )
-        ## Ubuntu
-    [ ! -e /usr/bin/php-fpm ] && [ -e "${PhpFpmNeve}" ] && ln -s ${PhpFpmNeve} /usr/bin/php-fpm
-
-    if [ ! -e /usr/bin/php-fpm ]; then
-	echo A php-fpm futtatóját nem sikerült beállítani...
-	exit 3
-    fi
-
+#	## A php-fpm futtatóját beállítom (nem kell: közvetlenül indítom)
+#    PhpFpmNeve=$( find /usr/sbin/ -executable|grep php-fpm )
+#        ## Ubuntu
+#    [ ! -e /usr/bin/php-fpm ] && [ -e "${PhpFpmNeve}" ] && ln -s ${PhpFpmNeve} /usr/bin/php-fpm
+#    if [ ! -e /usr/bin/php-fpm ]; then
+#	echo A php-fpm futtatóját nem sikerült beállítani...
+#	exit 3
+#    fi
 
     rm -rf /run && ln -s /tmp /run && mkdir /run/lock || exit 4
-
-    date +%Y-%m-%d 	>/defaults/BuildDate.txt
 
 	# Létrehozzuk az abc felhasználót
     /defaults/adduser-abc 	|| exit 5
 
+    date +%Y-%m-%d 	>/defaults/BuildDate.txt
 
     mkdir -p 		/config /downloads /defaults
     chown -RL abc:abc 	/config /downloads /defaults
