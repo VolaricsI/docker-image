300c94bf06cfd9a5a57b59b9047df49a000f3bcf
Volarics István <volaricsi@volarics.hu>
2020-04-09 18:23:03 +0200

    Az rtorrent image ellenorzes program lecserélése sh-ra, így nem kell perl az xmlrpc kommunikációhoz

--- rtorrent/defaults/configure.sh
+++ rtorrent/defaults/configure.sh
@@ -11,8 +11,8 @@
 
 	# futtatható programok beállítása és helyre mozgatása
     cd /defaults 	\
-    && chmod +x getupdate ellenorzes verzio plugins_disable plugins_install rtorrent-get-dir adduser-abc *.sh Stop 		\
-    && mv       getupdate ellenorzes verzio plugins_disable plugins_install rtorrent-get-dir Stop 			 /bin/ 	\
+    && chmod +x getupdate ellenorzes verzio plugins_disable plugins_install adduser-abc *.sh Stop 		\
+    && mv       getupdate ellenorzes verzio plugins_disable plugins_install Stop 			 /bin/ 	\
     && mv start.sh / 	\
     || exit 2
 
