e5772b558af8cf225869cf8d4ab26d0c7223579a
Volarics István <volaricsi@tu20.nker.volarics.hu>
2020-05-21 08:53:46 +0200

    Kell a PATH-ba

--- deluge/defaults/config.sh
+++ deluge/defaults/config.sh
@@ -2,7 +2,7 @@
 #
 #	Create by Voli
 
-    chmod +x /defaults/* && mv /defaults/start.sh / && mv /defaults/verzio /usr/bin/
+    chmod +x /defaults/* && mv /defaults/start.sh / && mv /defaults/verzio /usr/bin/ && mv /defaults/getupdate /usr/bin/
     /defaults/adduser-abc
 
     date +%Y-%m-%d 	>/defaults/BuildDate.txt
