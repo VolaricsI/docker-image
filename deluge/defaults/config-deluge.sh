31ba2615ebebca0573dbdb98a73865727eae334f
Volarics István <volaricsi@tu20.nker.volarics.hu>
2020-08-09 15:17:46 +0200

    Már 2.x-es verzió, bár még sokszor kifagy

--- deluge/defaults/config-deluge.sh
+++ deluge/defaults/config-deluge.sh
@@ -2,36 +2,42 @@
 #
 #	Create by Voli
 
-[ -f /config/auth  ] && exit 0 								## Ha van akkor nincs mit csinálni - van már config
+[ -f /config/auth  ] && exit 0 								## Ha van akkor nincs mit csinálni - van/lesz configja
 
 	D_USER=${DELUGE_USERNAME:=deluge}
 	D_PASS=${DELUGE_PASSWORD:=deluge}
 
+export LANG=en_US.UTF-8
+update-locale
+
 	echo "Doing initial setup..."
 
-	mkdir -p /config/state/
-	touch /config/state/torrents.state 						# Ne legyen hibaüzenet
+#	mkdir -p /config/state/
+#	touch /config/state/torrents.state 						# Ne legyen hibaüzenet
 
 	mkdir /config/In								# Innen fogja autómatán felemelni a torrenteket
 	chown -Rc abc:abc 	/config
-	chpst -u  abc deluged -c /config 						# A megfelelő user configuráljon
 
-	while [ ! -f /config/auth ]; do sleep 1; done 					# Wait until auth file created.
+##### EZ CSAK AZÉRT MERT NEM KEZELI A STRINGEKET A deluge-console ; MÉG JELENLEG
+	chpst -u  abc deluged -c /config 						# A megfelelő felhasználó configuráljon
+	while [ ! -f /config/core.conf ]; do sleep 1; done 				# Így biztos lesz konfigja
+	deluge-console -c /config "halt"; sleep 1					# Legyen ideje leállni
 
-	deluge-console -c /config "config -s allow_remote True"				# Nem csak a konténerből lehet managelni
+	sed -i 's|"download_location":.*,|"download_location": "/downloads",|'			/config/core.conf	# setup default paths to go to the user's defined data folder.
+	sed -i 's|"move_completed_path":.*,|"move_completed_path": "/downloads",|'		/config/core.conf
+	sed -i 's|"torrentfiles_location":.*,|"torrentfiles_location": "/config/Backup",|'	/config/core.conf
 
-	deluge-console -c /config "config -s download_location 		/downloads"	# setup default paths to go to the user's defined data folder.
-	deluge-console -c /config "config -s move_completed_path 	/downloads"
-	deluge-console -c /config "config -s torrentfiles_location 	/downloads"
-	deluge-console -c /config "config -s autoadd_enable 		true" 		# Felolvassa a torrent file-okat
-	deluge-console -c /config "config -s autoadd_location 		/config/In"
+##A deluge-console programmal 
+	chpst -u  abc deluged -c /config 						# A megfelelő felhasználó configuráljon
+	while [ ! -f /config/core.conf ]; do sleep 1; done 					# Wait until auth file created.
+
+	deluge-console -c /config "config -s allow_remote 		true"		# Nem csak a konténerből lehet managelni
 	deluge-console -c /config "config -s copy_torrent_file 		true" 		# A feldolgozott torrentet elteszi későbbre
-	deluge-console -c /config "config -s torrentfiles_location 	/config/Backup"
-
+	deluge-console -c /config "config -s auto_managed 		false"
 	deluge-console -c /config "config -s random_port 		false"
 
 	deluge-console -c /config "halt"; sleep 1					# Legyen ideje leállni
 
-	echo "Adding initial authentication details."
 
+	echo "Adding initial authentication details."
 	echo "${D_USER}:${D_PASS}:10" 	>>/config/auth
