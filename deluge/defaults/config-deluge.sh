1ce28f745ac44f96ad40cd2974dc4eb1035c6fc2
Volarics István <volaricsi@volarics.hu>
2021-04-17 18:43:09 +0200

    A string kezelés problémái

--- deluge/defaults/config-deluge.sh
+++ deluge/defaults/config-deluge.sh
@@ -7,9 +7,6 @@
 	D_USER=${DELUGE_USERNAME:=deluge}
 	D_PASS=${DELUGE_PASSWORD:=deluge}
 
-export LANG=en_US.UTF-8
-update-locale
-
 	echo "Doing initial setup..."
 
 #	mkdir -p /config/state/
@@ -18,26 +15,40 @@ update-locale
 	mkdir /config/In								# Innen fogja autómatán felemelni a torrenteket
 	chown -Rc abc:abc 	/config
 
-##### EZ CSAK AZÉRT MERT NEM KEZELI A STRINGEKET A deluge-console ; MÉG JELENLEG
-	chpst -u  abc deluged -c /config 						# A megfelelő felhasználó configuráljon
-	while [ ! -f /config/core.conf ]; do sleep 1; done 				# Így biztos lesz konfigja
-	deluge-console -c /config "halt"; sleep 1					# Legyen ideje leállni
+## Programozói hiba kijavítása....
+##	diff -u file1.html file2.html > patchfile.patch		!!!!!example
+##	Forrás: https://git.deluge-torrent.org/deluge/commit/?h=develop&id=d6c96d629183e8bab2167ef56457f994017e7c85
+#echo	patch /usr/lib/python3/dist-packages/deluge/i18n/util.py /defaults/patch/util.py.patch
+#	patch /usr/lib/python3/dist-packages/deluge/i18n/util.py /defaults/patch/util.py.patch
+##	Forrás: https://github.com/deluge-torrent/deluge/pull/268
+#echo	patch /usr/lib/python3/dist-packages/deluge/ui/console/cmdline/commands/config.py /defaults/patch/config.py.patch
+#	patch /usr/lib/python3/dist-packages/deluge/ui/console/cmdline/commands/config.py /defaults/patch/config.py.patch
+echo "1--------------------------------"
 
-	sed -i 's|"download_location":.*,|"download_location": "/downloads",|'			/config/core.conf	# setup default paths to go to the user's defined data folder.
-	sed -i 's|"move_completed_path":.*,|"move_completed_path": "/downloads",|'		/config/core.conf
-	sed -i 's|"torrentfiles_location":.*,|"torrentfiles_location": "/config/Backup",|'	/config/core.conf
+##### EZ CSAK AZÉRT MERT NEM KEZELI A STRINGEKET A deluge-console ; MÉG JELENLEG
+#	chpst -u  abc deluged -c /config 						# A megfelelő felhasználó configuráljon
+#	while [ ! -f /config/core.conf ]; do sleep 1; done 				# Így biztos lesz konfigja
+#	deluge-console -c /config "halt"; sleep 1					# Legyen ideje leállni
+#	sed -i 's|"download_location":.*,|"download_location": "/downloads",|'			/config/core.conf	# setup default paths to go to the user's defined data folder.
+#	sed -i 's|"move_completed_path":.*,|"move_completed_path": "/downloads",|'		/config/core.conf
+#	sed -i 's|"torrentfiles_location":.*,|"torrentfiles_location": "/config/Backup",|'	/config/core.conf
 
 ##A deluge-console programmal 
 	chpst -u  abc deluged -c /config 						# A megfelelő felhasználó configuráljon
-	while [ ! -f /config/core.conf ]; do sleep 1; done 					# Wait until auth file created.
+#	while [ ! -f /config/core.conf ]; do sleep 1; done 				# Wait until auth file created; így már elindult
+sleep 10
 
-	deluge-console -c /config "config -s allow_remote 		true"		# Nem csak a konténerből lehet managelni
-	deluge-console -c /config "config -s copy_torrent_file 		true" 		# A feldolgozott torrentet elteszi későbbre
+echo "2-------------------------------"
+	deluge-console -c /config "config -s allow_remote 		true" 		# Nem csak a konténerből lehet managelni
 	deluge-console -c /config "config -s auto_managed 		false"
 	deluge-console -c /config "config -s random_port 		false"
+	deluge-console -c /config "config -s copy_torrent_file 		true" 		# A feldolgozott torrentet elteszi későbbre
+## str változók és csak a patch után megy
+	deluge-console -c /config config -s download_location 		/downloads 	# Ide menti
+	deluge-console -c /config config -s move_completed_path 	/downloads 	# és itt is marad
+	deluge-console -c /config config -s torrentfiles_location 	/config/Backup 	# A torrent file-t ide is letárolja
 
 	deluge-console -c /config "halt"; sleep 1					# Legyen ideje leállni
-
 
 	echo "Adding initial authentication details."
 	echo "${D_USER}:${D_PASS}:10" 	>>/config/auth
