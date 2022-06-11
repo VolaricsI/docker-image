1355262105b090b4f766ba95df9b171d766b55fa
Volarics István <volaricsi@volarics.hu>
2022-06-11 07:23:09 +0200

    A php-fpm verzió függetlennl tétele

--- rtorrent/defaults/configure.sh
+++ rtorrent/defaults/configure.sh
@@ -23,11 +23,22 @@
     rm /var/log/nginx/error.log 	2>/dev/null; touch /tmp/nginx_error.log; 	ln -s /tmp/nginx_error.log 	/var/log/nginx/error.log
     rm /var/log/nginx/access.log 	2>/dev/null; touch /tmp/nginx_access.log; 	ln -s /tmp/nginx_access.log 	/var/log/nginx/access.log
 
-		## Ubuntu alatt máshol vannak
+		## A külömböző verziók miatt
     [ ! -e /sbin/chpst 		] && ln -s /usr/bin/chpst 	/sbin/chpst
-    [ ! -e /usr/sbin/php-fpm7 	] && ln -s $( ls /usr/sbin/php-fpm7*) /usr/sbin/php-fpm7
+
+    Bin=$( ls /usr/sbin/php-fpm* )
+    [ -e $Bin 	] && ln -s $Bin /defaults/php-fpm
+
     [ ! -e /sbin/chpst 		] && exit 40
-    [ ! -e /usr/sbin/php-fpm7 	] && exit 41
+    [ ! -e /defaults/php-fpm 	] && exit 41
+
+
+		## Ubuntu alatt máshol vannak
+#    [ ! -e /sbin/chpst 		] && ln -s /usr/bin/chpst 	/sbin/chpst
+#    [ ! -e /usr/sbin/php-fpm7 	] && ln -s $( ls /usr/sbin/php-fpm7*) /usr/sbin/php-fpm7
+#    [ ! -e /sbin/chpst 		] && exit 40
+#    [ ! -e /usr/sbin/php-fpm7 	] && exit 41
+
 
 
 #	## A php-fpm futtatóját beállítom (nem kell: közvetlenül indítom)
